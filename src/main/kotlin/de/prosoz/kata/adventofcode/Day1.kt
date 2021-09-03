d7485c3a95709708946f633de4db2df645f31879
Marc von Renteln <m.vonRenteln@prosoz.de>
2021-09-03 15:38:35 +0200

    Advent Of Code Day 1 - Simple Solution

--- src/main/kotlin/de/prosoz/kata/adventofcode/Day1.kt
+++ src/main/kotlin/de/prosoz/kata/adventofcode/Day1.kt
@@ -4,6 +4,21 @@ import java.io.File
 
 
 fun main() {
-    val inputs = File("src/main/resources/advent-of-code-input-day-1.txt").readLines()
-    println(inputs)
+    val inputs = File("src/main/resources/advent-of-code-input-day-1.txt")
+        .readLines()
+        .map { it.toInt() }
+
+    val output = productOfDay1(inputs)
+    print(output)
+}
+
+fun productOfDay1(inputs: List<Int>): Int? {
+    inputs.forEach { i ->
+        inputs.forEach { j ->
+            if (i + j == 2020) {
+                return i * j
+            }
+        }
+    }
+    return null
 }
---
+++ src/test/kotlin/de/prosoz/kata/adventofcode/Day1KtTest.kt
@@ -0,0 +1,21 @@
+package de.prosoz.kata.adventofcode
+
+import junit.framework.TestCase
+import org.junit.Test
+
+class Day1KtTest : TestCase() {
+
+    private val input = listOf<Int>(
+        1721,
+        979,
+        366,
+        299,
+        675,
+        1456
+    )
+
+    @Test
+    fun testDay1() {
+        assertEquals(514579, productOfDay1(input))
+    }
+}
